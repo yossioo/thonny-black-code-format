58dc23e71632e009190dafff1f3e10f43a87f184
Franccisco <35090042+Franccisco@users.noreply.github.com>
2019-10-06 14:06:47 -0300

    Added package structure.

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="thonny-black-format",
+    version="0.0.1",
+    author="Franccisco",
+    description="A plugin to format your python code with Black.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Franccisco/thonny-black-code-format",
+    packages=["thonnycontrib.thonny-black-format"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
---
+++ thonnycontrib/thonny-black-format/__init__.py
@@ -0,0 +1,63 @@
+from thonny import get_workbench
+from tkinter.messagebox import showinfo
+import subprocess
+
+name = "thonny-black-format"
+
+
+class BlackFormat:
+    workbench = get_workbench()
+
+    def __init__(self):
+        return self.load_plugin()
+
+    def format_black(self):
+        self.editor = self.workbench.get_editor_notebook().get_current_editor()
+        self.filename = self.editor.get_filename()
+        if self.filename is not None and self.filename[-3:] == ".py":
+            self.editor.save_file()
+            try:
+                format_code = subprocess.run(
+                    ["black", self.filename], capture_output=True, text=True
+                )
+            except FileNotFoundError:
+                final_title = "Error!"
+                final_message = (
+                    "Could not find black package. Is it installed and on your PATH?"
+                )
+            else:
+                if format_code.returncode != 0:
+                    final_title = "Error!"
+                    try:
+                        error_found = format_code.stderr.split("error:")[1].split("\n")[
+                            0
+                        ]
+                    except IndexError:
+                        error_found = format_code.stderr.split("Error:")[1].split("\n")[
+                            0
+                        ]
+
+                    final_message = error_found
+                else:
+                    self.editor._load_file(self.filename, keep_undo=True)
+                    final_title = "Success!"
+                    final_message = "Code Formated succesfully"
+
+        else:
+            final_title = "Error!"
+            final_message = "Looks like your file is not a python file"
+
+        showinfo(title=final_title, message=final_message)
+
+    def load_plugin(self):
+        self.workbench.add_command(
+            command_id="format_black",
+            menu_name="tools",
+            command_label="Format Black",
+            handler=self.format_black,
+            default_sequence="<Control-Alt-c>",
+            extra_sequences=["<<CtrlAltCInText>>"],
+        )
+
+
+run = BlackFormat()
