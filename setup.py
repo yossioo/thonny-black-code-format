fe7274cc7505b07a85c0ecfb9ec647b3d3f51b78
Franccisco <35090042+Franccisco@users.noreply.github.com>
2019-10-06 14:26:23 -0300

    Changed package name.

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="thonny-black-format",
+    name="thonny-black-code-format",
     version="0.0.1",
     author="Franccisco",
     description="A plugin to format your python code with Black in Thonny IDE.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Franccisco/thonny-black-code-format",
-    packages=["thonnycontrib.thonny-black-format"],
+    packages=["thonnycontrib.thonny-black-code-format"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
